a3da2a0aee6684f229ea2c95291f5350d58c77a4
Vlad Zhdanov <vladzhdanov@MacBook-Pro-Admin.local>
2018-12-09 16:23:46 +0200

    add nav

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -13,7 +13,6 @@ class App extends Component {
           
           <div className="d-flex home-screen flex-column justify-content-center text-center">
             <div className="home-screen__content">
-              <h1 className="brand-name">Dnipro Sky Lab</h1>
               <p className="home-screen__descriptions">Ініціативна група творчих людей, яких манить небо у всьому  своєму проявленні. Моделювання авіатехніки, ракетно-космічної техніки,  оптичних приладів, електронних систем – це ще неповний перелік інтересів  групи.</p>
             </div>
           </div>
@@ -22,7 +21,7 @@ class App extends Component {
             <div className="container flex-column h-100">
               <div className="row justify-content-between">
                 <div className="col">
-                  <h2 className="display-4">About us</h2>
+                  <h2 className="">About us</h2>
                 </div>
 
                 <div className="w-100 page-divider"></div>
@@ -46,7 +45,7 @@ class App extends Component {
             <div className="container flex-column h-100">
               <div className="row justify-content-between text-right">
                 <div className="col">
-                  <h2 className="display-4">Projects</h2>
+                  <h2 className="">Projects</h2>
                 </div>
 
                 <div className="w-100 page-divider"></div>
@@ -71,7 +70,7 @@ class App extends Component {
             <div className="container flex-column h-100">
               <div className="row justify-content-between">
                 <div className="col">
-                  <h2 className="display-4">Events</h2>
+                  <h2 className="">Events</h2>
                 </div>
 
                 <div className="w-100 page-divider"></div>
@@ -97,7 +96,7 @@ class App extends Component {
             <div className="container flex-column h-100">
               <div className="row justify-content-between text-right">
                 <div className="col">
-                  <h2 className="display-4">Expositions</h2>
+                  <h2 className="">Expositions</h2>
                 </div>
 
                 <div className="w-100 page-divider"></div>
@@ -127,7 +126,7 @@ class App extends Component {
 
             <div className="row">
               <div className="col">
-                <address id="contacts">
+                <address id="contacts" className="contacts">
                   <p><strong>Phone:</strong> +38 (123) 123-45-67</p>
                   <p><strong>Facebook:</strong> <a href="https://facebook.com" target="_blank">facebook.com</a></p>
                   <p><strong>YouTube:</strong> <a href="https://youtube.com" target="_blank">youtube.com</a></p>
